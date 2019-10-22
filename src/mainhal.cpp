2de0c8f73c1a2b12b92b67ab2e4e4e0384acc986
fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-10-22 20:46:46 +0800

    far away from completeness

--- src/mainhal.cpp
+++ src/mainhal.cpp
@@ -3,8 +3,20 @@
 //
 // Created by Fulva on 10/21/2019.
 //
-void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim) {}
+void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim) {
+
+}
 
 void EncoderInitialise() {
-
+            __GPIOA_CLK_ENABLE();
+            __GPIOB_CLK_ENABLE();
+            __TIM2_CLK_ENABLE();
+    TIM_HandleTypeDef timHandleTypeDef{.Instance=TIM2};
+    TIM_Encoder_InitTypeDef timEncoderInitTypeDef{.EncoderMode=TIM_ENCODERMODE_TI12,
+            .IC1Polarity=TIM_ICPOLARITY_BOTHEDGE,
+            .IC2Polarity=TIM_ICPOLARITY_BOTHEDGE};
+    HAL_TIM_Encoder_Init(
+            &timHandleTypeDef, &timEncoderInitTypeDef
+    );
+    HAL_TIM_Encoder_Start(&timHandleTypeDef,)
 }
